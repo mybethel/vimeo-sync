f652ae0eadeb303f0ba2d8cbb5c88b5340296bcf
Albert Martin <albert@imor.tl>
2016-12-26 14:23:28 -0500

    feat(sync): parallelize requests to additional pages of results

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const moment = require('moment');
-const request = require('request');
+const got = require('got');
 
 exports.handler = function(event, context) {
   const key = event.auth.key;
@@ -10,46 +10,74 @@ exports.handler = function(event, context) {
 
   const tags = event.tags.split(',').map(tag => tag.trim());
 
-  let matchingVideos = [];
-  getVideos(token, processPage(token, tags));
+  getVideos(token)
+    .then(checkPages(token, tags))
+    .then(context.succeed)
+    .catch(context.fail);
+};
 
-  /**
-   * Simple wrapper around the Vimeo API to query a user for all their videos.
-   * This hits the `/me/videos` endpoint on Vimeo which uses the `token` to
-   * determine which user to return videos for. On subsequent requests, the
-   * Vimeo API determines the endpoint to use providing pagination.
-   * @param {String} token User token for the Vimeo API.
-   * @param {Function} cb Callback once the request has been fulfilled.
-   * @param {String} pagination Optional parameter to paginate.
-   */
-  function getVideos(token, cb, pagination) {
-    const route = pagination || '/me/videos?page=1&per_page=50';
-    request.get({
-      url: `https://api.vimeo.com${route}`,
-      auth: {
-        bearer: token
-      },
-      headers: {
-        'User-Agent': 'Bethel Technologies <hello@bethel.io>'
-      },
-      json: true
-    }, cb);
+/**
+ * Simple wrapper around the Vimeo API to query a user for all their videos.
+ * This hits the `/me/videos` endpoint on Vimeo which uses the `token` to
+ * determine which user to return videos for.
+ * @param {String} token User token for the Vimeo API.
+ * @param {Function} cb Callback once the request has been fulfilled.
+ * @param {Number} page Optional page number for pagination.
+ */
+function getVideos(token, page) {
+  const route = `/me/videos?page=${page || 1}&per_page=50`;
+  return got(`https://api.vimeo.com${route}`, {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'User-Agent': 'Bethel Technologies <hello@bethel.io>'
+    },
+    json: true
+  });
+}
+
+/**
+ * Process the first page of results and determine how many pages to sync. This
+ * allows for the sync process to finish faster. When more pages are present,
+ * each additional page is fetched in parallel. Once all pages have been fetched
+ * every page of results is parsed.
+ * @param {String} token User token for the Vimeo API.
+ * @param {Array} tags All tags which should be used to match.
+ */
+function checkPages(token, tags) {
+  const tagsToSync = tags.toString().toLowerCase();
+
+  return function firstPageCallback(response) {
+    let additionalPages = 0;
+    if (response.body.total > 50) {
+      additionalPages = Math.ceil((response.body.total - 50) / 50);
+    }
+
+    let pageRequests = [];
+    for (var i = 2; i <= additionalPages + 1; i++) {
+      pageRequests.push(getVideos(token, i));
+    }
+
+    return Promise.all(pageRequests).then(results => {
+      results.unshift(response);
+      return processResults(results, tags);
+    });
   }
+}
 
-  /**
-   * Process a single page of Vimeo results and determine which videos match the
-   * criteria to sync. This is solely based on the tags, although by default any
-   * videos which have not been marked as public will be ignored.
-   * @param {String} token User token for the Vimeo API.
-   * @param {Array} tags All tags which should be used to match.
-   * @return {Function} A callback function used to page through additional
-   * results, or `context.succeed` when all pages have been exhausted.
-   */
-  function processPage(token, tags) {
-    const tagsToSync = tags.toString().toLowerCase();
+/**
+ * Process all pages of Vimeo results and determine which videos match the
+ * criteria to sync. This is solely based on the tags, although by default any
+ * videos which have not been marked as public will be ignored.
+ * @param {Array} results Each page of results returned from the Vimeo API.
+ * @param {Array} tags All tags which should be used to match.
+ */
+function processResults(results, tags) {
+  const tagsToSync = tags.toString().toLowerCase();
+  let matchingVideos = [];
 
-    return function pageCallback(err, response, body) {
-      matchingVideos = matchingVideos.concat(body.data.filter(video => {
+  return new Promise(resolve => {
+    results.forEach(response => {
+      matchingVideos = matchingVideos.concat(response.body.data.filter(video => {
         // If a video is not marked as public it is ignored. In the future this
         // could be modified to allow private channels to be used as a source.
         if (video.privacy && video.privacy.view !== 'anybody') {
@@ -65,14 +93,8 @@ exports.handler = function(event, context) {
         // If at least one tag matches, the video should be included.
         return matches.length > 0;
       }));
+    });
 
-      // If Vimeo has additional pages for us to parse, do that next.
-      if (body.paging && body.paging.next) {
-        return getVideos(token, processPage(token, tags), body.paging.next)
-      }
-
-      // Otherwise, return all matches.
-      context.succeed(matchingVideos);
-    }
-  }
-};
+    resolve(matchingVideos);
+  });
+}
